720f3e89bf74d58d50fc817e049277c20d6569c7
danygagnon <ddanygagnon@gmail.com>
2021-09-14 09:05:57 -0400

    refactor: single argument no parens

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const personnages = ["Bob", "Bobby", "Bobba", "Bongo", "Bingo"];
 function sync(list = personnages) {
     const listElement = document.getElementById("liste_personnages");
     const listItemHtml = list.map(
-        (element) => `
+        element => `
         <li class="list-group-item">
             ${element}
         </li>`
@@ -19,7 +19,7 @@ function addPersonnageFromInputName() {
 }
 
 function filter(recherche) {
-    const filteredPerso = personnages.filter((p) => p.includes(recherche));
+    const filteredPerso = personnages.filter(p => p.includes(recherche));
     sync(filteredPerso);
 }
 
